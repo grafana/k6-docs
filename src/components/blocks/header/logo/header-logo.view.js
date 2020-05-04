252dc214e67918a1334dabf1cf6949e61e41752f
Kirill Bolotsky <frontend@bolotsky.dev>
2020-05-04 22:41:25 +0500

    navigation updated according to latest design changes

--- src/components/blocks/header/logo/header-logo.view.js
+++ src/components/blocks/header/logo/header-logo.view.js
@@ -6,17 +6,20 @@ import styles from './header-logo.module.scss';
 
 import { main } from 'utils/urls';
 
-
-export const HeaderLogo = ({disableLink = false}) => {
+export const HeaderLogo = ({ disableLink = false, theme = 'default' }) => {
   if (disableLink) {
+    return <Logo className={styles.logo} />;
+  } else if (theme === 'doc') {
     return (
-      <Logo className={styles.logo}/>
-    );
-  } else {
-    return (
-      <a className={styles.wrapper} href={`${main}`}>
-        <Logo className={styles.logo}/>
-      </a>
+      <Link className={`${styles.wrapper} ${styles.wrapper_doc}`} to={`/`}>
+        <Logo className={styles.logo} />
+        <span>docs</span>
+      </Link>
     );
   }
+  return (
+    <a className={styles.wrapper} href={`${main}`}>
+      <Logo className={styles.logo} />
+    </a>
+  );
 };
