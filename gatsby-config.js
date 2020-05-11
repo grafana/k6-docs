0c0dd488ccba2988331e704ae83854087c30a6ed
Pepe Cano <825430+ppcano@users.noreply.github.com>
2020-05-11 02:12:15 +0200

    Upgrade `gatsby-plugin-algolia` and enable  `enablePartialUpdates`

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,6 +150,7 @@ if (
     options: {
       appId: process.env.GATSBY_ALGOLIA_APP_ID,
       apiKey: process.env.ALGOLIA_ADMIN_KEY,
+      enablePartialUpdates: true,
       queries,
       chunkSize: 10000, // default: 1000
     },
