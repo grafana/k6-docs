5eb91752f36556a4eefdcd2af0bda56ecb1d9d62
Kirill Bolotsky <frontend@bolotsky.dev>
2020-06-25 18:04:59 +0500

    removed page query and copypasted generation logic from 404; made 404 template, set up generation

---
+++ src/templates/404.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { DocLayout } from 'layouts/doc-layout';
+import { NotFound } from 'components/pages/404/not-found';
+import SeoMetadata from 'utils/seo-metadata';
+
+export default function ({ pageContext: { sidebarTree, navLinks } }) {
+  const pageMetadata = SeoMetadata[404];
+  return (
+    <DocLayout
+      sidebarTree={sidebarTree}
+      navLinks={navLinks}
+      pageMetadata={pageMetadata}
+    >
+      <NotFound />
+    </DocLayout>
+  );
+}
