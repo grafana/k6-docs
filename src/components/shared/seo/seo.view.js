d5abfc44c544f3dfba1704180bbb213011212301
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-02-26 13:47:51 +0300

    feat: add noindex robot tag for es pages

--- src/components/shared/seo/seo.view.js
+++ src/components/shared/seo/seo.view.js
@@ -38,50 +38,56 @@ export const SEO = ({
   const currentDescription = description || siteDescription;
   const currentUrl = slug ? `${docs}/${slug}` : docs;
   const currentImage = createMetaImagePath(image, siteUrl, siteImage);
-  return (
-    <Helmet
-      title={currentTitle}
-      htmlAttributes={{
-        lang: siteLanguage,
-        prefix: 'og: http://ogp.me/ns#',
-      }}
-    >
-      {/* General */}
-      <meta name={'description'} content={currentDescription} />
-      {/* Open Graph */}
-      <meta property={'og:url'} content={currentUrl} />
-      <meta property={'og:title'} content={currentTitle} />
-      <meta property={'og:description'} content={currentDescription} />
-      <meta property={'og:image'} content={currentImage} />
-      <meta property={'og:type'} content={'website'} />
-      {facebook && <meta property={'fb:app_id'} content={facebook.appId} />}
-      {/* Twitter Card tags */}
-      <meta name={'twitter:card'} content={'summary'} />
-      <meta name={'twitter:creator'} content={authorTwitterAccount} />
 
-      {/* SEO for localized pages */}
-      {/* rel should be declared after href https://github.com/nfl/react-helmet/issues/279 */}
-      {pageTranslations && pageTranslations.en !== undefined && (
-        <link
-          hrefLang="en"
-          href={`${siteUrl}/docs${pageTranslations.en.path}`}
-          rel="alternate"
-        />
+  return (
+    <>
+      {slug && slug.startsWith('es/') && (
+        <Helmet meta={[{ name: 'robots', content: 'noindex' }]} />
       )}
-      {pageTranslations && pageTranslations.es !== undefined && (
-        <link
-          hrefLang="es"
-          href={`${siteUrl}/docs${pageTranslations.es.path}`}
-          rel="alternate"
-        />
-      )}
-      {pageTranslations && (
-        <link
-          hrefLang="x-default"
-          href={`${siteUrl}/${currentUrl}`}
-          rel="alternate"
-        />
-      )}
-    </Helmet>
+      <Helmet
+        title={currentTitle}
+        htmlAttributes={{
+          lang: siteLanguage,
+          prefix: 'og: http://ogp.me/ns#',
+        }}
+      >
+        {/* General */}
+        <meta name={'description'} content={currentDescription} />
+        {/* Open Graph */}
+        <meta property={'og:url'} content={currentUrl} />
+        <meta property={'og:title'} content={currentTitle} />
+        <meta property={'og:description'} content={currentDescription} />
+        <meta property={'og:image'} content={currentImage} />
+        <meta property={'og:type'} content={'website'} />
+        {facebook && <meta property={'fb:app_id'} content={facebook.appId} />}
+        {/* Twitter Card tags */}
+        <meta name={'twitter:card'} content={'summary'} />
+        <meta name={'twitter:creator'} content={authorTwitterAccount} />
+
+        {/* SEO for localized pages */}
+        {/* rel should be declared after href https://github.com/nfl/react-helmet/issues/279 */}
+        {pageTranslations && pageTranslations.en !== undefined && (
+          <link
+            hrefLang="en"
+            href={`${siteUrl}/docs${pageTranslations.en.path}`}
+            rel="alternate"
+          />
+        )}
+        {pageTranslations && pageTranslations.es !== undefined && (
+          <link
+            hrefLang="es"
+            href={`${siteUrl}/docs${pageTranslations.es.path}`}
+            rel="alternate"
+          />
+        )}
+        {pageTranslations && (
+          <link
+            hrefLang="x-default"
+            href={`${siteUrl}/${currentUrl}`}
+            rel="alternate"
+          />
+        )}
+      </Helmet>
+    </>
   );
 };
