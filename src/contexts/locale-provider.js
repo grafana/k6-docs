a16e448317a952ac7ab9ea344d8583e8b4f54fb0
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-03-08 18:37:30 +0300

    fix: save url language in local storage if there is no value for it yet

--- src/contexts/locale-provider.js
+++ src/contexts/locale-provider.js
@@ -7,21 +7,17 @@ export const useLocale = () => {
 
 export default function LocaleProvider({ urlLocale = 'en', children }) {
   const initLocale =
-    typeof localStorage !== 'undefined'
+    typeof localStorage !== 'undefined' && localStorage.getItem('k6-doc-locale')
       ? localStorage.getItem('k6-doc-locale')
       : urlLocale;
   const curLocaleRef = useRef(initLocale);
   const [renderKey, setRenderKey] = useState({});
 
-  // console.log('LocaleProvider.render', { locale: curLocaleRef.current });
-
   useEffect(() => {
-    // console.log('LocaleProvider', { urlLocale });
     if (
       typeof localStorage !== 'undefined' &&
       !localStorage.getItem('k6-doc-locale')
     ) {
-      // console.log('LocaleProvider', 'setting LS');
       localStorage.setItem('k6-doc-locale', urlLocale);
     }
   }, [urlLocale]);
@@ -29,15 +25,10 @@ export default function LocaleProvider({ urlLocale = 'en', children }) {
   const localeContextValue = useMemo(() => {
     return {
       get locale() {
-        // console.log('localeContextValue.get', { locale: curLocaleRef.current });
         return curLocaleRef.current;
       },
       urlLocale,
       setLocale: (locale) => {
-        // console.log('localeContextValue.set', {
-        //   locale,
-        //   curLocale: curLocaleRef.current,
-        // });
         if (typeof localStorage !== 'undefined') {
           localStorage.setItem('k6-doc-locale', locale);
         }
