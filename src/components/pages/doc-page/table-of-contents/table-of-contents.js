eca20401f5e84a96a746c538ac51c5cf0efd3380
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-03-16 12:13:10 +0300

    feat: update ToC styles, add h3 headers to ToC

--- src/components/pages/doc-page/table-of-contents/table-of-contents.js
+++ src/components/pages/doc-page/table-of-contents/table-of-contents.js
@@ -7,6 +7,7 @@ import styles from './table-of-contents.module.scss';
 
 const components = {
   h2: HeadingLandmark('h2'),
+  h3: HeadingLandmark('h3'),
 };
 
 const TableOfContents = forwardRef(
@@ -22,7 +23,7 @@ const TableOfContents = forwardRef(
     const links = useLandmark(
       {
         containerRef: contentContainerRef,
-        markSelector: 'h2',
+        markSelector: 'h2, h3',
       },
       [],
     );
@@ -53,10 +54,14 @@ const TableOfContents = forwardRef(
       >
         <nav className={`${styles.anchorBar} ${label ?? ''}`}>
           <ul className={styles.anchorWrapper}>
-            {links.map(({ title, anchor }, i) => (
+            {links.map(({ title, anchor, tagName }, i) => (
               <li className={styles.anchorBox} key={`al-${i}`}>
                 <a
-                  className={classNames(styles.anchor)}
+                  className={
+                    tagName === 'H2'
+                      ? classNames(styles.h2)
+                      : classNames(styles.h3)
+                  }
                   href={`${anchor}`}
                   onClick={(e) => handleAnchorClick(e, anchor)}
                 >
