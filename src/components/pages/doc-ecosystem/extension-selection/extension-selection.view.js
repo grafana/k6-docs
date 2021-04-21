556a354f9d3b592d54da17e36ebaa4692cff4e38
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-04-21 16:28:44 +0300

    feat: add extensions data

--- src/components/pages/doc-ecosystem/extension-selection/extension-selection.view.js
+++ src/components/pages/doc-ecosystem/extension-selection/extension-selection.view.js
@@ -1,102 +1,13 @@
 import { Code } from 'components/shared/code';
 import { ExtensionCard } from 'components/shared/extension-card';
+import EXTENSIONS_DATA from 'data/ecosystem/extensions';
 import React, { useState } from 'react';
 
 import styles from './extension-selection.module.scss';
 
-const data = {
-  extensions: [
-    {
-      name: 'Kafka',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka2',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka3',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka4',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka5',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka6',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-  ],
-};
-
 export const ExtensionSelection = () => {
-  const { extensions } = data;
   const [selected, setSelected] = useState(
-    Array(extensions.length).fill(false),
+    Array(EXTENSIONS_DATA.length).fill(false),
   );
 
   const handleCheckboxClick = (index) => {
@@ -107,7 +18,7 @@ export const ExtensionSelection = () => {
 
   // TODO: always use most recent k6 version
   let code = '$ xk6 build v0.31.0';
-  extensions.forEach((extension, index) => {
+  EXTENSIONS_DATA.forEach((extension, index) => {
     if (selected[index]) {
       code += ` --with ${extension.url.replace('https://', '')}`;
     }
@@ -121,7 +32,7 @@ export const ExtensionSelection = () => {
         </Code>
       </div>
       <div className={styles.list}>
-        {extensions.map((extension, index) => (
+        {EXTENSIONS_DATA.map((extension, index) => (
           <ExtensionCard
             key={extension.name}
             extension={extension}
--- src/components/pages/doc-ecosystem/extensions-list/extensions-list.view.js
+++ src/components/pages/doc-ecosystem/extensions-list/extensions-list.view.js
@@ -1,102 +1,13 @@
 import { ExtensionCard } from 'components/shared/extension-card';
+import EXTENSIONS_DATA from 'data/ecosystem/extensions';
 import React from 'react';
 
 import styles from './extensions-list.module.scss';
 
-const data = {
-  extensions: [
-    {
-      name: 'Kafka',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka2',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka3',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka4',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka5',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-    {
-      name: 'Kafka6',
-      description: 'Load test Apache Kafka. With support for Avro messages',
-      url: 'https://github.com/mostafa/xk6-kafka',
-      docs: 'https://xk6kafka.example.org',
-      issues: 'https://github.com/mostafa/xk6-kafka/issues',
-      logo: 'https://url.for.some/image.jpg',
-      author: {
-        name: 'Mostafa Moradian',
-        url: 'https://github.com/mostafa',
-      },
-      official: false,
-      categories: ['Clients', 'Protocols'],
-    },
-  ],
-};
-
 export const ExtensionsList = () => {
-  const { extensions } = data;
   return (
     <section className={`container ${styles.container}`}>
-      {extensions.map((extension) => (
+      {EXTENSIONS_DATA.map((extension) => (
         <ExtensionCard key={extension.name} extension={extension} />
       ))}
     </section>
