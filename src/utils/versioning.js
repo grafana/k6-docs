36c32367ac44fbb2649dac27967aa7a8be8766a3
Ivan Mirić <ivan@loadimpact.com>
2021-06-29 17:35:40 +0200

    Explain DEFAULT_JS_API_VERSIONS_TO_BUILD

--- src/utils/versioning.js
+++ src/utils/versioning.js
@@ -3,6 +3,7 @@ const SUPPORTED_VERSIONS = ['v0.31', 'v0.32'];
 /** latest version number for URLs without version prefix */
 const LATEST_VERSION = 'v0.33';
 
+/** applies only for development and staging; all versions are built on production */
 const DEFAULT_JS_API_VERSIONS_TO_BUILD = 2;
 
 module.exports = {
