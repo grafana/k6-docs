d1066d0a277b209e45775e730cf2b1388b5b382d
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-08-31 19:44:25 +0300

    feat: update archive version script to remove replacing urls

--- archive-js-api-version.sh
+++ archive-js-api-version.sh
@@ -11,6 +11,3 @@ mkdir "./versioned-js-api/$VERSION/javascript\ api"
 
 # copy content of docs/javascript api to the version folder
 cp -pr "./docs/02 javascript api/." "./versioned-js-api/$VERSION/javascript api/"
-
-# replace internal links for javascript-api section (/javascript-api/ => /javascript-api/v0-XX/)
-find "./versioned-js-api/$VERSION/javascript api" -type f -iname "*.md" -exec sed -i -e "s,/javascript-api/,/${VERSION}/javascript-api/,g" {} \;
