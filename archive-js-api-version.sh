117bc483bab87123d4b3e0404cef262853b6b8bd
Pepe Cano <825430+ppcano@users.noreply.github.com>
2021-09-01 17:20:52 +0200

    Fix `archive-js-api-version` bash script to create correctly the versioned folder

--- archive-js-api-version.sh
+++ archive-js-api-version.sh
@@ -7,7 +7,7 @@ cd src/data/markdown
 
 # create folder for archived version data
 mkdir "./versioned-js-api/$VERSION"
-mkdir "./versioned-js-api/$VERSION/javascript\ api"
+mkdir "./versioned-js-api/$VERSION/javascript api"
 
 # copy content of docs/javascript api to the version folder
 cp -pr "./docs/02 javascript api/." "./versioned-js-api/$VERSION/javascript api/"
