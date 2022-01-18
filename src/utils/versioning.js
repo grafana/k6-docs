e573aa661e6506a9440e3b58c885627c9a2b7dad
Oleg Bespalov <oleg.bespalov@grafana.com>
2022-01-18 12:52:08 +0100

    Archive v0.35 documentation and switch the latest version

--- src/utils/versioning.js
+++ src/utils/versioning.js
@@ -1,7 +1,7 @@
 /** version number for which documentation is available (except for the latest version) */
-const SUPPORTED_VERSIONS = ['v0.31', 'v0.32', 'v0.33', 'v0.34'];
+const SUPPORTED_VERSIONS = ['v0.31', 'v0.32', 'v0.33', 'v0.34', 'v0.35'];
 /** latest version number for URLs without version prefix */
-const LATEST_VERSION = 'v0.35';
+const LATEST_VERSION = 'v0.36';
 
 /** applies only for development and staging; all versions are built on production */
 const DEFAULT_JS_API_VERSIONS_TO_BUILD = 2;
