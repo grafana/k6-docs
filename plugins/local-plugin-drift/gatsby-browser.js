bb708eeda5b63547f02035cb1d74efce34fe6375
Maksim Gorodov <silencer.web@gmail.com>
2022-04-25 19:11:55 +0400

    refactor: fix ESLint errors

--- plugins/local-plugin-drift/gatsby-browser.js
+++ plugins/local-plugin-drift/gatsby-browser.js
@@ -3,7 +3,9 @@ const utils = require('utils');
 exports.onInitialClientRender = () => {
   if (utils.isInIFrame()) {
     return;
-  } else if (typeof drift === 'object' && window.drift && window.driftAppId) {
+  }
+
+  if (typeof drift === 'object' && window.drift && window.driftAppId) {
     window.drift.SNIPPET_VERSION = '0.3.1';
     window.drift.load(window.driftAppId);
 
@@ -16,10 +18,10 @@ exports.onInitialClientRender = () => {
     // hide default drift icon on load
     window.drift.on('ready', (api) => {
       api.widget.hide();
-      window.drift.on('sidebarClose', (e) => {
+      window.drift.on('sidebarClose', () => {
         document.removeEventListener('click', sidebarClickOutside);
       });
-      window.drift.on('sidebarOpen', (e) => {
+      window.drift.on('sidebarOpen', () => {
         document.addEventListener('click', sidebarClickOutside);
       });
     });
