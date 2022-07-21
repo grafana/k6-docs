56e2da4442d005d1737d64936f1a56c8f132f3e6
Mac#3 <denbratch@gmail.com>
2022-07-21 12:00:38 +0500

    feat: add blockquote updates

--- src/components/shared/blockquote/blockquote.view.js
+++ src/components/shared/blockquote/blockquote.view.js
@@ -2,24 +2,41 @@ import classNames from 'classnames';
 import React from 'react';
 
 import styles from './blockquote.module.scss';
+import AttentionIcon from './svg/attention.inline.svg';
+import InfoIcon from './svg/pencil-note.inline.svg';
+import WarningIcon from './svg/warning.inline.svg';
 
-const Blockquote = ({ children, mod = 'default' }) => {
+const Blockquote = ({ children, title, mod = 'default' }) => {
   // create optional kicker in case of modifications
   const getKicker = (mod) => {
     const kickers = {
-      warning: 'warning!',
+      attention: 'attention',
+      info: 'info',
+      warning: 'warning',
       default: '',
     };
-    return kickers[mod] ? (
-      <span className="kicker">{kickers[mod]}</span>
-    ) : (
-      kickers[mod]
+
+    const icons = {
+      attention: AttentionIcon,
+      info: InfoIcon,
+      warning: WarningIcon,
+    };
+
+    const Icon = icons[mod] ?? null;
+
+    return (
+      <div className={styles.wrapper}>
+        {Icon && <Icon className={styles.icon} src={icons[mod]} />}
+        <span className="kicker">{!title ? kickers[mod] : title}</span>
+      </div>
     );
   };
   return (
     <blockquote
       className={classNames({
         [styles.docBlockquoteWarning]: mod === 'warning',
+        [styles.docBlockquoteAttention]: mod === 'attention',
+        [styles.docBlockquoteInfo]: mod === 'info',
       })}
     >
       {getKicker(mod)}
