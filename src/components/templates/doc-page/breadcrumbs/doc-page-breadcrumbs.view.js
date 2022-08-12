18326dee167785557f7ea587a8623d6afd5de039
Kirill Bolotsky <frontend@bolotsky.dev>
2022-08-12 16:11:52 +0300

    fix: breadcrumbs revisit

--- src/components/templates/doc-page/breadcrumbs/doc-page-breadcrumbs.view.js
+++ src/components/templates/doc-page/breadcrumbs/doc-page-breadcrumbs.view.js
@@ -6,14 +6,18 @@ import styles from './doc-page-breadcrumbs.module.scss';
 export const Breadcrumbs = (props) => {
   const { items, label } = props;
   return (
-    <div className={`${styles.wrapper} ${label}`}>
-      {items.map((item) => (
-        <div key={item.path} className={styles.itemContainer}>
-          <Link className={styles.item} to={item.path}>
-            {item.name}
-          </Link>
-        </div>
+    <ul className={`${styles.wrapper} ${label}`}>
+      {items.map((item, idx) => (
+        <li key={item.path} className={styles.itemContainer}>
+          {idx === items.length - 1 ? (
+            <span className={styles.item}>{item.name}</span>
+          ) : (
+            <Link className={styles.item} to={item.path}>
+              {item.name}
+            </Link>
+          )}
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
