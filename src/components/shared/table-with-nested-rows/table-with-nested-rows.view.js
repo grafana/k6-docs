9cb7552234db74a7eb8321282ada15c731780c20
Matt Dodson <47385188+MattDodsonEnglish@users.noreply.github.com>
2022-08-20 00:39:58 -0400

    feat: add component TableWithNestedRows

--- src/components/shared/table-with-nested-rows/table-with-nested-rows.view.js
+++ src/components/shared/table-with-nested-rows/table-with-nested-rows.view.js
@@ -1,16 +1,13 @@
 import classNames from 'classnames/bind';
 import TableWrapper from 'components/shared/table-wrapper';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 
-import { CodeInline } from '../code';
-
-import CollapsibleClosedIcon from './svg/collapsible-closed.inline.svg';
-import CollapsibleOpenIcon from './svg/collapsible-open.inline.svg';
 import styles from './table-with-nested-rows.module.scss';
 
 const cx = classNames.bind(styles);
 
+<<<<<<< HEAD
 /* 1.
  * Parse the array of children
  * to get flat array with id being the child index in original array
@@ -42,13 +39,47 @@ const parseRows = (rows) =>
       group =
         children[0]?.props?.children?.props?.children ||
         children[0]?.props?.children;
+=======
+function parseRows(rows) {
+  return rows.map(
+    ({ props: { children } }) =>
+      children[0]?.props?.children?.props?.children ||
+      children[0]?.props?.children,
+  );
+}
+
+function getCollapsibleRowIndexes(rows) {
+  const parsedRows = parseRows(rows);
+  const collapsibleRowIndexes = {};
+
+  parsedRows.forEach((startRow, startRowIndex) => {
+    if (!startRow.includes('.')) {
+      const nestedRowIndexes = [];
+
+      for (
+        let nestedRowIndex = 0;
+        nestedRowIndex < parsedRows.length;
+        nestedRowIndex += 1
+      ) {
+        if (nestedRowIndex > startRowIndex) {
+          if (
+            new RegExp(`^${startRow}\\..*`).test(parsedRows[nestedRowIndex])
+          ) {
+            nestedRowIndexes.push(nestedRowIndex);
+          } else {
+            break;
+          }
+        }
+      }
+
+      if (nestedRowIndexes.length > 0) {
+        collapsibleRowIndexes[startRowIndex] = nestedRowIndexes;
+      }
+>>>>>>> 95e979e2 (feat: add component TableWithNestedRows)
     }
-    return {
-      id: i,
-      group: group.trim(),
-    };
   });
 
+<<<<<<< HEAD
 const structureRows = (parsedRows) => {
   const result = [];
   const hash = { _: result };
@@ -219,27 +250,79 @@ const changeExpandedAtId = (id) => (row) => {
     };
   return row;
 };
+=======
+  return collapsibleRowIndexes;
+}
+
+function checkIsActive(activeParentRowIndexes, rowIndex) {
+  return activeParentRowIndexes.includes(rowIndex);
+}
+
+function checkIsExpanded(index, collapsibleRowIndexes, activeParentRowIndexes) {
+  const isChildRow = Object.values(collapsibleRowIndexes).some((childrenRows) =>
+    childrenRows.includes(index),
+  );
+
+  if (!isChildRow) return true;
+
+  if (activeParentRowIndexes.length === 0) return false;
+
+  return activeParentRowIndexes.some((activeParentRowIndex) =>
+    collapsibleRowIndexes[activeParentRowIndex].includes(index),
+  );
+}
+>>>>>>> 95e979e2 (feat: add component TableWithNestedRows)
 
 const TableBody = ({ children }) => {
-  const [rowsState, setRowsState] = useState(() =>
-    structureRows(parseRows(children)),
+  const [activeParentRowIndexes, setActiveParentRowIndexes] = React.useState(
+    [],
   );
-  const handleToggleClick = (rowId) =>
-    setRowsState((rows) => rows.map(changeExpandedAtId(rowId)));
+
+  const collapsibleRowIndexes = getCollapsibleRowIndexes(children);
+
+  const handleToggleButtonClick = (rowIndex) => {
+    setActiveParentRowIndexes((activeParentRowIndexes) =>
+      activeParentRowIndexes.includes(rowIndex)
+        ? activeParentRowIndexes.slice(
+            0,
+            activeParentRowIndexes.indexOf(rowIndex),
+          )
+        : [...activeParentRowIndexes, rowIndex],
+    );
+  };
+
   return (
     <tbody>
-      {rowsState.map((row, idx) =>
-        renderRowsGroup({
-          handleToggleClick,
-          data: children,
-          row,
-          rowIdx: idx,
-          isLast: idx === rowsState.length - 1,
-          isCollapsible: row.length > 1,
-          isExpanded: row?.[0]?.isExpanded ?? false,
-          nestLevel: 0,
-        }),
-      )}
+      {children.map(({ props: { children } }, rowIndex) => {
+        const isCollapsible = collapsibleRowIndexes[rowIndex]?.length > 0;
+        const isActive = checkIsActive(activeParentRowIndexes, rowIndex);
+        const isExpanded = checkIsExpanded(
+          rowIndex,
+          collapsibleRowIndexes,
+          activeParentRowIndexes,
+        );
+
+        if (!isExpanded) return null;
+
+        return (
+          <tr key={rowIndex}>
+            {children.map(({ props: { children } }, cellIndex) => (
+              <td key={cellIndex}>
+                {children}
+                {cellIndex === 0 && isCollapsible && (
+                  <button
+                    className={cx('toggle-button')}
+                    type="button"
+                    onClick={() => handleToggleButtonClick(rowIndex)}
+                  >
+                    {isActive ? 'Collapse' : 'Expand'}
+                  </button>
+                )}
+              </td>
+            ))}
+          </tr>
+        );
+      })}
     </tbody>
   );
 };
