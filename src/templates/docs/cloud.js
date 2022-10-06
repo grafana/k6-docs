a9dfd34576dd839728ad1e6f287d90cdfe090da7
Matt Dodson <47385188+MattDodsonEnglish@users.noreply.github.com>
2022-10-06 11:22:41 -0300

    Change embedded video

--- src/templates/docs/cloud.js
+++ src/templates/docs/cloud.js
@@ -53,7 +53,7 @@ export default function Cloud({ pageContext: { sidebarTree, navLinks } }) {
                   <iframe
                     width="560"
                     height="315"
-                    src="https://www.youtube.com/embed/ncxCIuo5tUU"
+                    src="https://www.youtube.com/embed/gwO7k5RTE54"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
