95871fa7c12460352a1218724d6f676decf4c7a8
Matt Dodson <47385188+MattDodsonEnglish@users.noreply.github.com>
2022-11-29 15:25:45 -0300

    lint WIP component

--- src/components/shared/browser-docs-wip/browser-docs-wip.view.js
+++ src/components/shared/browser-docs-wip/browser-docs-wip.view.js
@@ -4,8 +4,8 @@ import React from 'react';
 const BrowserDocsWIP = () => (
   <Blockquote mod="attention">
     <p>
-      This API is a work in progress. Some of the following
-      functionalities might behave unexpectedely.
+      This API is a work in progress. Some of the following functionalities
+      might behave unexpectedely.
     </p>
   </Blockquote>
 );
