b74308b4e410b0b229d410b5e8ac1c19309661ff
Jack Baldry <jack.baldry@grafana.com>
2023-03-06 09:39:10 +0000

    Mount docs at /docs/k6
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- grafana-cloud/README.md
+++ grafana-cloud/README.md
@@ -11,7 +11,7 @@ Then follow these steps:
 
 1. Run `make docs` (or `sudo make docs`, depending on the necessary Docker privileges).
 1. Enter to confirm.
-1. Preview docs at [http://localhost:3002/docs/k6-in-grafana/](http://localhost:3002/docs/k6-in-grafana/).
+1. Preview docs at [http://localhost:3002/docs/k6-in-grafana/](http://localhost:3002/docs/k6/).
 
 The process builds the site using the [docs-base container image](https://hub.docker.com/r/grafana/docs-base).
 The presentation you see at `localhost` depends on what the latest version of that image is.
--- grafana-cloud/docs/docs.mk
+++ grafana-cloud/docs/docs.mk
@@ -5,39 +5,53 @@ export SHELLOPTS := pipefail:errexit
 MAKEFLAGS += --warn-undefined-variables
 MAKEFLAGS += --no-builtin-rule
 
-DOCS_IMAGE   = grafana/docs-base:latest
-DOCS_PROJECT = k6-in-grafana
-DOCS_DIR     = sources
+GIT_ROOT := $(shell git rev-parse --show-toplevel)
+# Support podman over Docker if it is available.
+PODMAN  := $(shell if command -v podman >/dev/null 2>&1; then echo podman; else echo docker; fi)
 
 # This allows ports and base URL to be overridden, so services like ngrok.io can
 # be used to share a local running docs instances.
-DOCS_HOST_PORT    = 3002
-DOCS_LISTEN_PORT  = 3002
-DOCS_BASE_URL    ?= "localhost:$(DOCS_HOST_PORT)"
+DOCS_HOST_PORT   := 3002
+DOCS_LISTEN_PORT := 3002
+DOCS_BASE_URL    := "localhost:$(DOCS_HOST_PORT)"
+
+PROJECT         := k6
+PROJECT_VERSION :=
+ifeq ($(PROJECT_VERSION),)
+PROJECT_URL         := http://$(DOCS_BASE_URL)/docs/$(PROJECT)/
+PROJECT_CONTENT_DIR := /hugo/content/docs/$(PROJECT)
+else
+PROJECT_URL         := http://$(DOCS_BASE_URL)/docs/$(PROJECT)/$(PROJECT_VERSION)
+PROJECT_CONTENT_DIR := /hugo/content/docs/$(PROJECT)/$(PROJECT_VERSION)
+endif
+
+DOCS_IMAGE     := grafana/docs-base:latest
+DOCS_CONTAINER := $(PROJECT)-docs
 
 HUGO_REFLINKSERRORLEVEL ?= WARNING
-DOCS_DOCKER_CONTAINER = $(DOCS_PROJECT)-docs
 
-.PHONY: docs-docker-rm
-docs-docker-rm:
-	docker rm -f $(DOCS_DOCKER_CONTAINER)
+.PHONY: docs-rm
+docs-rm: ## Remove the docs container.
+	$(PODMAN) rm -f $(DOCS_CONTAINER)
 
 .PHONY: docs-pull
-docs-pull:
-	docker pull $(DOCS_IMAGE)
+docs-pull: ## Pull documentation base image.
+	$(PODMAN) pull $(DOCS_IMAGE)
 
 .PHONY: docs
 docs: ## Serve documentation locally.
 docs: docs-pull
 	@echo "Documentation will be served at:"
-	@echo "http://$(DOCS_BASE_URL)/docs/$(DOCS_PROJECT)/"
-	@echo ""
-	@if [[ -z $${NON_INTERACTIVE} ]]; then \
-		read -p "Press a key to continue"; \
+	echo $(PROJECT_URL)
+	echo ""
+	if [[ -z $${NON_INTERACTIVE} ]]; then \
+		read -p "Press enter to continue"; \
 	fi
-	docker run -it --name $(DOCS_DOCKER_CONTAINER) \
-		-v $(CURDIR)/$(DOCS_DIR):/hugo/content/docs/$(DOCS_PROJECT)/:rw,z \
+	$(PODMAN) run -ti \
+		--init \
+		-v $(GIT_ROOT)/docs/sources:$(PROJECT_CONTENT_DIR):ro,z \
 		-e HUGO_REFLINKSERRORLEVEL=$(HUGO_REFLINKSERRORLEVEL) \
 		-p $(DOCS_HOST_PORT):$(DOCS_LISTEN_PORT) \
-		--rm $(DOCS_IMAGE) \
-		make server
+		--name $(DOCS_CONTAINER) \
+		--rm \
+		$(DOCS_IMAGE) make server
