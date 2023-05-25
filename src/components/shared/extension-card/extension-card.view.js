424dcf5cfe5514bb4ddd6c9eb6124b8800a22a15
Raman Yasel <raman.yasel@gmail.com>
2023-05-25 17:47:26 +0300

    chore: update extensions card link

--- src/components/shared/extension-card/extension-card.view.js
+++ src/components/shared/extension-card/extension-card.view.js
@@ -5,9 +5,26 @@ import {
   styles as itemCardStyles,
 } from 'components/shared/item-card';
 import React from 'react';
-import ExternalLinkIcon from 'svg/external-link.inline.svg';
 
 import styles from './extension-card.module.scss';
+
+const parseString = (str) => {
+  let string = str.replace(/-/gi, '<wbr />-');
+
+  // Find the last occurrence of '<wbr />'
+  const lastIndex = string.lastIndexOf('<wbr />');
+
+  if (lastIndex !== -1) {
+    // Wrap the part of the string after the last '<wbr />' with 'span' tags
+    const prefix = string.slice(0, lastIndex);
+    const suffix = string.slice(lastIndex + 8); // Length of '<wbr />'
+
+    // eslint-disable-next-line max-len
+    string = `${prefix}<wbr />-<span class="no-wrap">${suffix}<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none" style="margin-left: 8px"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M4.429 1H2.714C1 1 1 1 1 2.714v9.352C1 13 1 13 2.714 13h8.572C13 13 13 13 13 11.286V9.57M7.857 1H13m0 0v5.143M13 1 6.143 7.857"/></svg></span>`;
+  }
+
+  return string;
+};
 
 export const ExtensionCard = ({
   extension,
@@ -33,7 +50,12 @@ export const ExtensionCard = ({
   const Wrapper = ({ className = '', children }) => {
     if (hasCheckbox) {
       return (
-        <ItemCard liftOnHover={false} as="div">
+        <ItemCard
+          liftOnHover={false}
+          as="button"
+          type="button"
+          onClick={onCheckboxClick}
+        >
           <div
             className={`${styles.withCheckbox} ${
               isChecked ? styles.checked : ''
@@ -70,9 +92,14 @@ export const ExtensionCard = ({
           </div>
         )}
         <div className={styles.nameWrapper}>
-          <span
+          <a
             className={styles.name}
-            dangerouslySetInnerHTML={{ __html: extensionName }}
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            dangerouslySetInnerHTML={{
+              __html: parseString(extensionName),
+            }}
           />
         </div>
         <span
@@ -99,15 +126,6 @@ export const ExtensionCard = ({
           {cloudEnabled && (
             <span className={styles.cloud}>Available in cloud</span>
           )}
-          <a
-            className={styles.link}
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>External link</span>
-            <ExternalLinkIcon aria-hidden />
-          </a>
         </div>
       </div>
     </Wrapper>
