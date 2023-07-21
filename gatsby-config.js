3d1a645f5575bf40164e7762a566d81169b2746a
Imma Valls <immavalls@grafana.com>
2023-07-21 17:49:02 +0200

    bypass es-lint length

--- gatsby-config.js
+++ gatsby-config.js
@@ -184,8 +184,12 @@ if (shouldAnnouncementBannerBeShown) {
         __dirname,
         './src/components/shared/announcement-banner/announcement-banner.js',
       ),
-      text: '🎓 We are excited to announce that, starting on July 25th at 14:00 UTC, we will merge the k6 community forum with the Grafana community forum',
-      link: 'https://k6.io/blog/k6-forum-migration/',
+      text:
+        // eslint-disable-next-line max-len
+        '🎓 We are excited to announce that, starting on July 25th at 14:00 UTC, we will merge the k6 community forum with the Grafana community forum',
+      link:
+        // eslint-disable-next-line max-len
+        'https://k6.io/blog/k6-forum-migration/',
       buttonText: 'Learn more',
     },
     // settings below have to match
