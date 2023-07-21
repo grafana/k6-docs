44787aa4bfabb2dec7423b5219bd2523a8802c81
Imma Valls <imma.valls@grafana.com>
2023-07-21 18:09:37 +0200

    update emoji

--- gatsby-config.js
+++ gatsby-config.js
@@ -186,7 +186,7 @@ if (shouldAnnouncementBannerBeShown) {
       ),
       text:
         // eslint-disable-next-line max-len
-        'We are excited to announce that, starting on July 25th at 14:00 UTC, we will merge the k6 community forum with the Grafana community forum',
+        '🎉 We are excited to announce that, starting on July 25th at 14:00 UTC, we will merge the k6 community forum with Grafana',
       link:
         // eslint-disable-next-line max-len
         'https://k6.io/blog/k6-forum-migration/',
