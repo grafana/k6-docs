d12f76385798815518795d2c8da27d760c256718
Théo Crevon <oleiade@users.noreply.github.com>
2023-08-10 14:46:15 +0200

    Apply suggestions from code review
    Co-authored-by: Heitor Tashiro Sergent <heitortsergent@gmail.com>

--- src/components/shared/blocking-aws-blockquote/blocking-aws-blockquote.view.js
+++ src/components/shared/blocking-aws-blockquote/blocking-aws-blockquote.view.js
@@ -9,11 +9,11 @@ const BlockingAwsBlockquote = () => (
     In some cases, using this library&apos;s operations might impact
     performance and skew your test results. <br />
     <br />
-    To ensure accurate results, consider executing these operations in the
+    To ensure accurate results, consider executing these operations in the{' '}
     <span className="code-inline">setup</span> and{' '}
     <span className="code-inline">teardown</span>{' '}
     <a href="/docs/using-k6/test-lifecycle/">lifecycle functions</a>. These
-    functions run before and after the test run and thus do not influence test
+    functions run before and after the test run and have no impact on the test
     results.
   </Blockquote>
 );
