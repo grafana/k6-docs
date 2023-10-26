7c4851860aff886097067de88211bdb8539acb28
Pepe Cano <825430+ppcano@users.noreply.github.com>
2023-10-26 16:21:17 +0200

    Set `canonicalUrl` for non-markdown pages

--- src/utils/seo-metadata.js
+++ src/utils/seo-metadata.js
@@ -12,6 +12,7 @@ export default {
   'javascript-api': {
     data: {
       title: 'k6 JavaScript API',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       slug: '/javascript-api/',
       description:
         'This page provides the documentation of the k6 JavaScript API.',
@@ -20,13 +21,23 @@ export default {
   extensions: {
     data: {
       title: 'k6 Extensions',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'The extension ecosystem enables developers and testers to extend k6 to cover use cases not supported natively in the core. Explore the endless possibilities of k6 and xk6.',
+    },
+  },
+  'explore-extensions': {
+    data: {
+      title: 'k6 Extensions',
+      canonicalUrl: 'https://grafana.com/docs/k6',
+      description:
+        'Discover the k6 extensions available to meet your specific needs..',
     },
   },
   'bundle-builder': {
     data: {
       title: 'k6 Bundle Builder',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'Easily create your own bespoke k6 binary with all the extensions you want to run using the bundle builder and xk6.',
     },
@@ -34,6 +45,7 @@ export default {
   examples: {
     data: {
       title: 'k6 Examples & Tutorials',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'The k6 Examples & Tutorials is a directory with common k6 examples and the most popular tutorials using k6.',
     },
@@ -41,6 +53,7 @@ export default {
   cloud: {
     data: {
       title: 'k6 Cloud Documentation',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'The k6 Cloud documentation helps you in your usage of the k6 Cloud. The k6 Cloud is the perfect companion to k6 with an aim to bring ease-of-use and convenience to scale your performance testing efforts.',
     },
@@ -48,6 +61,7 @@ export default {
   guides: {
     data: {
       title: 'k6 Documentation',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'The k6 Documentation helps you to use k6 to get your performance testing on the right track. Learn more about load and performance testing. Get started in minutes.',
     },
@@ -56,6 +70,7 @@ export default {
     data: {
       title:
         'Documentación de k6 - Herramienta Open Source de Pruebas de Carga',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'La documentación de k6 le ayudará a ejecutar sus pruebas de rendimiento correctamente. Aprenda y comience rápidamente a efectuar sus tests de carga y rendimiento',
     },
@@ -63,6 +78,7 @@ export default {
   integrations: {
     data: {
       title: 'k6 Integrations',
+      canonicalUrl: 'https://grafana.com/docs/k6',
       description:
         'The k6 Integrations page lists the most popular k6 integrations. Integrate k6 with CI tools, store results in different services, select different Grafana dashboards, or use converters for the auto-generation of your load test.',
     },
