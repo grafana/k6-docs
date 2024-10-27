7efd4b8e66a0032586c6dda50268cf50e4805a10
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2024-10-27 19:44:37 -0300

    Revert "Split publishing workflows (#1781)"
    This reverts commit ac65e3036ed803aa3aaef9dd3d93827523024158.

--- docs/variables.mk
+++ docs/variables.mk
@@ -1,7 +1,9 @@
 # List of projects to provide to the make-docs script.
 # Format is PROJECT[:[VERSION][:[REPOSITORY][:[DIRECTORY]]]]
-# The following PROJECTS value mounts content into the k6 and k6-studio projects.
-# This results in the content being served at http://localhost:3002/docs/k6/ and http://localhost:3002/docs/k6-studio/.
-# The sources are found relative to thecurrent repository checkout determined by the name of the directory of the Git root.
+# The following PROJECTS value mounts content into the k6 project.
+# This results in the content being served at http://localhost:3002/docs/k6/.
+# The source of the content is the `/docs/sources` directory, which is the script default if not explicitly set.
+# The `docs/sources` directory is found within the current repository checkout determined by the name of the directory of the git root.
 # This overrides the default behavior of assuming the repository directory is the same as the project name.
-PROJECTS := k6-studio:UNVERSIONED:$(notdir $(basename $(shell git rev-parse --show-toplevel))):docs/sources/k6-studio k6:UNVERSIONED:$(notdir $(basename $(shell git rev-parse --show-toplevel))):docs/sources/k6
+PROJECTS := k6:UNVERSIONED:$(notdir $(basename $(shell git rev-parse --show-toplevel)))
+export WEBSITE_MOUNTS := true
