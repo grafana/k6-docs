754f0fbc0937f1f870e24b46e28068431fc1aea5
Jack Baldry <jack.baldry@grafana.com>
2025-02-14 10:21:56 +0000

    Restore eslint mdx code checking for Markdown sources
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/sources/.eslintrc.js
+++ docs/sources/.eslintrc.js
@@ -9,13 +9,30 @@ module.exports = {
     sourceType: 'module',
     requireConfigFile: false, // <== ADD THIS
   },
-  extends: ['plugin:prettier/recommended'],
+  extends: ['plugin:mdx/recommended', 'plugin:prettier/recommended'],
   plugins: ['markdown'],
   overrides: [
     {
       // Enable the Markdown processor for all .md files.
       files: ['**/*.md'],
       processor: 'markdown/markdown',
+    },
+    {
+      // Customize the configuration ESLint uses for ```js, ```javascript
+      files: ['**/*.md/*.js', '**/*.md/*.javascript'],
+      rules: {
+        'no-undef': 'error',
+        'no-else-return': 'error',
+        'no-extra-boolean-cast': 'error',
+        'no-duplicate-case': 'error',
+        'no-const-assign': 'error',
+        'no-dupe-args': 'error',
+        'no-dupe-else-if': 'error',
+        'no-var': 'error',
+        'use-isnan': 'error',
+        'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+        'prefer-const': 'error',
+      },
     },
   ],
   globals: {
