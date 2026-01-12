df24444fa0ba7920f2647504a0e6a8de16e21eaf
Jack Baldry <jack.baldry@grafana.com>
2026-01-12 15:35:15 +0000

    Mount k6-studio as a versioned project
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -4,6 +4,6 @@
 # This results in the content being served at http://localhost:3002/docs/k6/ and http://localhost:3002/docs/k6-studio/.
 # The sources are found relative to thecurrent repository checkout determined by the name of the directory of the Git root.
 # This overrides the default behavior of assuming the repository directory is the same as the project name.
-PROJECTS := k6-studio:UNVERSIONED:$(notdir $(basename $(shell git rev-parse --show-toplevel))):docs/sources/k6-studio k6:UNVERSIONED:$(notdir $(basename $(shell git rev-parse --show-toplevel))):docs/sources/k6
+PROJECTS := k6-studio::$(notdir $(basename $(shell git rev-parse --show-toplevel))):docs/sources/k6-studio k6:UNVERSIONED:$(notdir $(basename $(shell git rev-parse --show-toplevel))):docs/sources/k6
 
 export WEBSITE_MOUNTS := true
